1eb0311b2f0a845e699cc66a90a048878ac90a5e
Julia Paul <juliapaulmyla@yahoo.com>
2025-03-23 23:32:11 -0400

    update promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,37 +26,47 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(200).json(books);
+  const getBooks = new Promise((resolve, rej) => {
+    resolve(res.status(200).json(books))
+  })
+  getBooks.then(() => console.log('book list returned'))
 });
 
-// Task 11
-// const bookResult = axios.get('/').then(data => data).catch(err => console.error(err))
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const {isbn} = req.params
-  const bookResult = books[isbn]
-  if(bookResult) {
-    return res.status(200).json(bookResult);
-  } else
-    return res.status(404).json({message: "No book found!"});
+  const getBooks = new Promise((resolve, rej) => {
+    const bookResult = books[isbn]
+    if(bookResult)
+      resolve(res.status(200).json(bookResult))
+    else
+      rej(res.status(404).send('no book found!'))
+  })
+
+  getBooks.then(() => console.log('book list returned'))
+  
  });
 
-//  Task 11
-//  const bookResult2 = axios.get('/isbn/2').then(data => data).catch(err => console.error(err))
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const {author} = req.params
-  let bookResult;
-  for(let book in books) {
-    if(books[book].author === author)
-      bookResult = books[book]
-  }
-  if(bookResult) {
-    return res.status(200).json(bookResult);
-  } else
-    return res.status(404).json({message: "No book found!"});
+
+  const getBooks = new Promise((resolve, rej) => {
+    let bookResult;
+    for(let book in books) {
+      if(books[book].author === author)
+        bookResult = books[book]
+    }
+    if(bookResult) {
+      resolve(res.status(200).json(bookResult))
+    } else
+      rej(res.status(404).json({message: "No book found!"}))
+    })
+
+    getBooks.then(() => console.log('book list returned'))
+
 });
 
 // task 12
@@ -65,15 +75,21 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   const {title} = req.params
-  let bookResult;
-  for(let book in books) {
-    if(books[book].title === title)
-      bookResult = books[book]
-  }
-  if(bookResult) {
-    return res.status(200).json(bookResult);
-  } else
-    return res.status(404).json({message: "No book found!"});
+
+  const getBooks = new Promise((resolve, rej) => {
+    let bookResult;
+    for(let book in books) {
+      if(books[book].title === title)
+        bookResult = books[book]
+    }
+    if(bookResult) {
+      resolve(res.status(200).json(bookResult))
+    } else
+      rej(res.status(404).json({message: "No book found!"}))
+    })
+
+    getBooks.then(() => console.log('book list returned'))
+
 });
 
 // Task 13
